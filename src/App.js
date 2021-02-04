b3d97ee58286b9b38638a5f1749dee266233ac3b
Warakorn <boydoubleb@gmail.com>
2021-02-04 15:07:20 +0700

    https://github.com/boy-warakorn/barcode-scanner.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import { BrowserMultiFormatReader } from '@zxing/library';
 
-function App() {
+const App = () => {
+  const [deviceId, setDeviceId] = useState('');
+  const [resultText, setResultText] = useState('NOT FOUND');
+  let codeReader = useRef();
+  useEffect(() => {
+    codeReader.current = new BrowserMultiFormatReader();
+
+    codeReader.current.listVideoInputDevices((vidInDe) => {
+      console.log(vidInDe);
+      setDeviceId(vidInDe[0].deviceId);
+    });
+  }, []);
+
+  useEffect(() => {
+    codeReader.current.reset();
+  }, [resultText]);
+
+  const onRead = () => {
+    codeReader.current.decodeFromVideoDevice(deviceId, 'video', (result) => {
+      if (result) {
+        setResultText(result.text);
+      }
+    });
+    console.log(`Started continous decode from camera with id ${deviceId}`);
+  };
+
+  const onStop = () => {
+    codeReader.current.reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video
+        id='video'
+        width='300'
+        height='200'
+        style={{ border: 'solid black 1px' }}
+      ></video>
+      <div className='result'>{resultText}</div>
+      <button onClick={onRead}>CLICK</button>
+      <button onClick={onStop}>CLICK</button>
     </div>
   );
-}
+};
 
 export default App;
